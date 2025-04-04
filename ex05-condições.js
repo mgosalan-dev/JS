aa214d8c2a1f5fcbe99ac0c352666ca305e011f2
mgosalan-dev <mgosalan.dev@gmail.com>
2025-04-04 21:17:18 +0100

    exemplos de js

---
+++ ex05-condições.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`A velocidade do seu carro é de ${vel}km/h'`)
+
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida de 60km/h. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando sinto de segurança`)
---
+++ ex06-condições-composta.js
@@ -0,0 +1,9 @@
+var pais = 'Portugal';
+
+console.log(`Vivendo em ${pais}`);
+
+if (pais == 'Brasil') { // Condição composta
+    console.log('Você é Brasileiro');
+} else { 
+    console.log('Você é Estrangeiro');
+}
