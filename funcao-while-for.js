056a01400860de87b14d5639af5b5ba771a529f2
mgosalan-dev <mgosalan.dev@gmail.com>
2025-04-15 00:58:35 +0100

    Atualização

---
+++ funcao-while-for.js
@@ -0,0 +1,22 @@
+console.log('exemplo em while')
+var c = 1
+while (c <= 5){
+    console.log(`teste de ${c}`)
+    c++
+}
+
+console.log('')
+console.log('exemplo em for')
+console.log('')
+
+for(var b = 1; b <= 5; b++){
+    console.log(`Teste em for ${b}`)
+    
+
+}
+
+/*
+    for é um instrumento de verificação ele vem com inicio, o teste e o incremento {} 
+
+    o codigo é o mesmo porem com estilo de escrita diferentes a mais usada é o for pois é mais limpo
+*/
--- funcao-while.js
+++ funcao-while.js
@@ -1,5 +1,19 @@
 var c = 1
-while (c <= 500){ // esse é um laço simprles
+while (c <= 3) { // esse é um laço simprles
     console.log(`Passo ${c}`)
     c++ // isso representa c+1 , c+2 até da 8 certinho
 }
+console.log('')
+console.log('Proximo Exemplo ')
+console.log('')
+
+// estrutura do
+
+var b = 1
+do {
+    console.log(`Passo ${b}`)
+    b++
+} while (b <= 6)
+
+console.log()
+console.log('é mais comum vermos a versão while pois é mais limpa e direta')
